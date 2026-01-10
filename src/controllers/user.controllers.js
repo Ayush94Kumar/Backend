df7ae24b6f958f9bc72da6aac32292932671ef1d
Ayush kumar <ayush76671kumar@gmail.com>
2026-01-10 11:50:20 +0530

    register user using controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,82 @@
-import {asyncHandler} from "../utils/asynHandler.js"
+import { asyncHandler } from "../utils/asynHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.models.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 
-const registerUser = asyncHandler(async (req, res) =>{
-    res.status(200).json({
-        message : "raju bhai"
-    })
+const registerUser = asyncHandler(async (req, res) => {
+   //get user details from frontend
+   //validation - not empty
+   //check if user already exists : username, email
+   //check for image ,check for avtar
+   //upload them to cloudinary, avatar
+   //create user object- create entry in db
+   //remove password and refresh token field from response
+   //check for user creation 
+   //retrun res
+
+   //get user details from frontend
+   const { fullName, email, username, password } = req.body
+   console.log("email:", email);
+
+   //validation - not empty
+   if (
+      [fullName, email, username, password].some((field) =>
+         field?.trim() === "")
+   ) {
+      throw new ApiError(400, "all fileds required");
+   }
+
+   //check if user already exists : username, email
+   const existedUser = User.findOne({
+      $or: [{ username }, { email }]
+   })
+   if (existedUser) {
+      throw new ApiError(409, "User with email and username exist")
+   }
+
+   //check for image ,check for avtar
+   const avatarLocalPath = req.files?.avatar[0]?.path;
+   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+   if (!avatarLocalPath) {
+      throw new ApiError(400, "avatar is must requred");
+   }
+
+   //upload them to cloudinary, avatar
+   const avatar = await uploadOnCloudinary(avatarLocalPath)
+   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+   if (!avatar) {
+      throw new ApiError(400, "avatar is must requred");
+   }
+
+   //create user object- create entry in db
+   const user = await User.create({
+      fullName,
+      avatar: avatar.url,
+      coverImage: coverImage?.url || "",
+      email,
+      password,
+      username: username.toLowerCase()
+   })
+
+   //remove password and refresh token field from response
+   const createdUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+   )
+
+   //check for user creation 
+   if (!createdUser) {
+      throw new ApiError(500, "Somthing went wrong to register user")
+   }
+
+   //retrun res
+   return res.status(201).json(
+      new ApiResponse(200, createdUser, "User register Successfully")
+   )
+
+
 })
 
-export {registerUser,}
+export { registerUser, }
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -52,7 +52,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -60,30 +60,30 @@ userSchema.method.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
-userSchema.method.generateAccessToken =  function () {
+userSchema.method.generateAccessToken = function () {
     return jwt.sign(
         {
-        _id: this._id,
-        username: this.username,
-        email: this.email,
-        fullname: this.fullname,
-    },
-    process.env.ACCESS_TOKEN_SECRET,
-    {
-        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
-    }
-)
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+            fullname: this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    )
 }
 
-userSchema.method.generateRefreshToken =  function () {
+userSchema.method.generateRefreshToken = function () {
     return jwt.sign({
         _id: this._id,
     },
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
-    }
-)
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    )
 }
 
 
